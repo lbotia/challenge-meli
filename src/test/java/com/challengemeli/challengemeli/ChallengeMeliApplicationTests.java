9e826d0889f6c0a74ecc9435e8bd3289649571c7
Laura  Botia <laurabotia@Lauras-MacBook-Air.local>
2022-07-07 19:56:28 -0500

    First Commit Test Meli

---
+++ src/test/java/com/challengemeli/challengemeli/ChallengeMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengemeli.challengemeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeMeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
